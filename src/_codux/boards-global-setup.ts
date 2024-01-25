a1951e75ea2a202e50eda27fd2da90a7105fc476
serhiiz <serhiiz@wix.com>
2024-01-25 11:56:17 +0100

    add bluepring

--- src/_codux/boards-global-setup.ts
+++ src/_codux/boards-global-setup.ts
@@ -3,3 +3,6 @@ import '../styles/_color.scss';
 import '../styles/_radius.scss';
 import '../styles/_spacing.scss';
 import '../styles/_type.scss';
+
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
